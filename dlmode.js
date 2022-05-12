9f1c0fedd2485a231e8b0611c1cdad072bb2fec2
mdsjjorge <mdsousa.jorge@gmail.com>
2022-05-12 14:17:20 -0300

    separa JS de HTML

---
+++ dlmode.js
@@ -0,0 +1,12 @@
+function darkMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "dark-mode";
+    content.innerText = "Dark Mode is ON";
+  }
+  function lightMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "light-mode";
+    content.innerText = "Dark Mode is OFF";
+  }
